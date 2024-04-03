e8b563a8b7e67102c61366c6c136a56dcc5dc1d9
Meshmanuu <meshanyangu@gmail.com>
2024-04-03 14:16:34 +0300

    lab done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    // index.js
+
+// Bind an event listener to the button with id 'button'
+document.getElementById('button').addEventListener('click', function() {
+    // Event listener logic goes here
+  });
+  
 }
+  
